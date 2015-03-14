e12f51c882ec16e855a404912016c4ca6e295047
Bhadresh Patel <bhadreshp05@gmail.com>
2015-03-14 18:05:33 -0500

    updating user model

--- app/models/user.js
+++ app/models/user.js
@@ -1,34 +1,36 @@
-// grab the packages that we need for the user model
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var bcrypt = require('bcrypt-nodejs');
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+var bcrypt 		 = require('bcrypt-nodejs');
 
-// user Schema
-var UserSchema = new Schema({
+// user schema
+var UserSchema   = new Schema({
 	name: String,
 	username: { type: String, required: true, index: { unique: true }},
 	password: { type: String, required: true, select: false }
 });
 
 // hash the password before the user is saved
-UserSchema.pre('save', function (next) {
+UserSchema.pre('save', function(next) {
 	var user = this;
 
-	// has the password only if the password has been changed or user is new
+	// hash the password only if the password has been changed or user is new
 	if (!user.isModified('password')) return next();
 
 	// generate the hash
-	bcrypt.hash(user.password, null, null, function (err, hash) {
+	bcrypt.hash(user.password, null, null, function(err, hash) {
+		if (err) return next(err);
+
+		// change the password to the hashed version
 		user.password = hash;
 		next();
 	});
 });
 
-// method to compare a given password with the datebase hash
-UserSchema.methods.comparePassword = function (password) {
+// method to compare a given password with the database hash
+UserSchema.methods.comparePassword = function(password) {
 	var user = this;
+
 	return bcrypt.compareSync(password, user.password);
 };
 
-// return the model
 module.exports = mongoose.model('User', UserSchema);
